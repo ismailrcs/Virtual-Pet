7d39292ad39230fbb0ac9529caaae1c2794b7a62
ismailrcs <69443263+ismailrcs@users.noreply.github.com>
2020-11-05 12:53:01 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,62 +1,95 @@
 
-var database;
-var gameState = 0;
-var playerCount;
-var form,game,player;
-var allPlayer;
-var car1,car2,car3,car4;
+var dog, happyDog
 
-var car1img , car2img , car3img , car4img;
-var track , ground;
+var database,milk;
+
+var food, foodStock;
+
+var dogImg,happyDogImg;
 
 
-var cars;
+function preload()
+{
+  dogImg = loadImage('images/dogImg.png');
 
-function preload(){
+  happyDogImg = loadImage("images/dogImg1.png");
 
-    car1img = loadImage("images/car1.png");
-    car2img = loadImage("images/car2.png");
-    car3img = loadImage("images/car3.png");
-    car4img = loadImage("images/car4.png");
-    ground = loadImage("images/ground.png");
-    track = loadImage("images/track.jpg");
+}
 
+  function setup() {
+    createCanvas(500, 500);
+  
+
+    dog = createSprite(250,250,50,50);
+    happyDog = createSprite(250,250,50,50);
+  
+    dog.addImage(dogImg);
+    happyDog.addImage(happyDogImg);
+    dog.scale = 0.2;
+    happyDog.scale = 0.2;
+    dog.visible = true;
+    happyDog.visible = false;
+
+    database = firebase.database();
+
+  }
+
+
+function draw() {  
+
+  background(46, 139, 87);
+
+  if(keyIsDown(UP_ARROW)){
+
+    writeStock(food);
+
+    dog.visible = false;
+    happyDog.visible = true;
+
+  }
+
+ foodStock = database.ref('Food');
+  
+  foodStock.on("value" , readStock)
+
+
+  drawSprites();
+
+  fill("white");
+
+  textSize(16);
+
+  stroke("red");
+
+  text(" NOTE : Press UP_ARROW TO FEED THE DOG " , 100,100);
+
+  
+
+}
+
+  function readStock(data){
+
+    food = data.val();
+
+  }
+
+  function writeStock(x1){
+
+    if(x1<=0){
+
+      x1 = 0;
+
+    }
+  else{
+    x1 = x1-1;
+    }
+
+   database.ref('/').update({
+
+    Food : x1
+      
+    })
   
   }
 
-function setup(){
-    createCanvas(windowWidth-70,windowHeight-70);
-    
-   database = firebase.database();
 
-    game = new Game();
-    
-    game.getState();
-
-    game.start();
-
-
-}
-
-function draw(){
-    
-
-if(playerCount == 4 && gameState === 0){
-
-    game.updateState(1);
-}
-    
-if( gameState == 1){
-
-    background(ground);
-    image(track,0,-4*windowHeight,windowWidth,5*windowHeight);
-    game.play();
-    drawSprites();
-}
-if( gameState === 2){
-
-    game.end();
-
-}
-
-}
